5f59015ce3fd8df362f776a5118e704da4931a79
Zubair Siddiqui <60069297+sodmzs@users.noreply.github.com>
2020-01-20 19:10:03 +0530

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,18 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from accounts.views import dashboardView, profile, registerView
 # Create your tests here.
 
+class TestUrls(SimpleTestCase):
+
+	def test_dashboard_view(self):
+		url = reverse('home')
+		self.assertEquals(resolve(url).func, dashboardView)
+
+	def test_register_view(self):
+		url = reverse('register')
+		self.assertEquals(resolve(url).func, registerView)
+
+	def test_profile_view(self):
+		url = reverse('profile')
+		self.assertEquals(resolve(url).func, profile)
